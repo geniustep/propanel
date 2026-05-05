fc05ae2186ef5ee53fc25170a35dac275568e4c4
geniustep <agsteps@gmail.com>
2026-05-05 21:54:54 +0100

    new

--- src/lib/site-contact.ts
+++ src/lib/site-contact.ts
@@ -2,6 +2,9 @@
 
 export const SITE_EMAIL = "contact@propanel.ma";
 
+/** Affichage (numéro marocain) */
+export const SITE_PHONE_LOCAL_DISPLAY = "0661339892";
+
 export const SITE_PHONE_TEL_HREF = "tel:+212661339892";
 
 /** wa.me : indicatif pays + national sans le 0 initial */
